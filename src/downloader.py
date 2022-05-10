7b20013d891dc64cbd755d8f14562fb6cea32e82
Elisabeth S. Friedrich <elisabeth.s.friedrich@gmail.com>
2022-05-10 08:54:05 +0200

    inline: Prevent urls from being lowercased
    most webpages don't have a problem with that - except for example
    Instagram. The Video-ID is case sensitive.

--- src/downloader.py
+++ src/downloader.py
@@ -193,8 +193,8 @@ class Downloader:
 
     @staticmethod
     def _get_url(url: str, info: Dict[str, Any]) -> str:
-        url = url.lower()
-        if "vm.tiktok.com" in url and "webpage_url" in info:
+        t_url = url.lower()
+        if "vm.tiktok.com" in t_url and "webpage_url" in info:
             # remove the share tracking from the tiktok video
             # by extracting the pure URL
             new_url = info["webpage_url"]
