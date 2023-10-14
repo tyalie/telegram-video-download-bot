c4ad408d0f9c322f373bd77d8d324150e7c16340
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2023-10-14 02:22:35 +0200

    update: deps: Upgrade yt-dlp to latest version
    With this yt-dlp has been upgraded to 2023.10.13, from an almost 1 year
    old one. This resolves a few issues around YouTube downloads not
    working.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- src/plugins/youtube_dl_injection.py
+++ src/plugins/youtube_dl_injection.py
@@ -3,19 +3,23 @@ from yt_dlp.compat import compat_basestring
 from yt_dlp.utils import sanitized_Request
 
 
-def _custom_urlopen(self, req):
-    """ Start an HTTP download """
-    if isinstance(req, compat_basestring):
-        req = sanitized_Request(req)
+def _custom_urlopen(original):
 
-    if self.params.get("http_headers"):
-        # stolen from YoutubeDLHandler.http_request
-        for h, v in self.params.get("http_headers").items():
-            if h.capitalize() not in req.headers:
-                req.add_header(h, v)
+    def _open(self, req):
+        """ Start an HTTP download """
+        if isinstance(req, compat_basestring):
+            req = sanitized_Request(req)
 
-    return self._opener.open(req, timeout=self._socket_timeout)
+        if self.params.get("http_headers"):
+            # stolen from YoutubeDLHandler.http_request
+            for h, v in self.params.get("http_headers").items():
+                if h.capitalize() not in req.headers:
+                    req.update(headers={h: v})
 
+        return original(self, req)
+
+    return _open
 
 YoutubeDL2 = YoutubeDL
-YoutubeDL2.urlopen = _custom_urlopen
+original_open = YoutubeDL2.urlopen
+YoutubeDL2.urlopen = _custom_urlopen(original_open)
